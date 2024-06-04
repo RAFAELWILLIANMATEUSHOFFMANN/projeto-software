2379902e12f33301cc5ef9e57d9f9512bb1de757
RAFAELWILLIANMATEUSHOFFMANN <rafael.wmh@hotmail.com>
2024-06-03 22:21:12 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <footer class="rodapé">
-    <p>rodapé</p>
-  </footer>
+  <div>
+    <HelloWorld />
+    <ListaClientes />
+    <Cabecalho />
+    <BarraLateral />
+    <!-- Outros componentes aqui -->
+    <Formulario />
+    <Rodape />
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorldNew.vue'
 import ListaClientes from './components/ListaClientes.vue'
-import Cabeçalho from './components/Cabeçalho.vue'
+import Cabecalho from './components/MeuCabeçalho.vue'
 import BarraLateral from './components/BarraLateral.vue'
 import Formulario from './components/Formulario.vue'
 import Rodape from './components/Rodape.vue'
@@ -15,13 +21,13 @@ import Rodape from './components/Rodape.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    ListaClientes,
-    Cabeçalho,
-    BarraLateral,
-    Formulario,
-    Rodape
-  }
+  HelloWorld,
+  ListaClientes,
+  Cabecalho,
+  BarraLateral,
+  Formulario,
+  Rodape
+}
 }
 </script>
 
